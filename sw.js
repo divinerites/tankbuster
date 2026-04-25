83a7aae94940b286076fa34f0358a657681a2be0
Didier Divinerites <divinerites@gmail.com>
2026-04-25 10:51:28 +0200

    Make a PWA webapp

---
+++ sw.js
@@ -0,0 +1,13 @@
+// sw.js - service worker minimal pour My Poker ShotClock
+
+self.addEventListener('install', event => {
+  // on passe immédiatement à l'étape suivante
+  self.skipWaiting();
+});
+
+self.addEventListener('activate', event => {
+  // on prend le contrôle des pages ouvertes
+  event.waitUntil(self.clients.claim());
+});
+
+// Pas de logique de cache ici : la PWA fonctionne uniquement en ligne
